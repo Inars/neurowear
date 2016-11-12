302e3ff0438a20e1a870d9e1ee76f18059ef01a2
Elie <eliesaad1995@gmail.com>
2016-11-12 02:10:20 +0200

    Third of many...

--- js/header.js
+++ js/header.js
@@ -1,13 +1,23 @@
 window.onload = function () {
-    var canvas = document.getElementsByClassName("canvas-header");
-    var ctx = canvas.getContext("2D");
-    var radius = canvas.height / 2;
-    ctx.translate(radius, radius);
-    drawMagnifier(ctx, radius);
+    drawMagnifier();
 };
 
-function drawMagnifier(ctx, radius){
-    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "transparent";
+function drawMagnifier(){
+    var canvas = document.getElementsByClassName("canvas-header")[0];
+    var ctx = canvas.getContext("2d");
+    var radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius *= 0.65;
+    ctx.beginPath();
+    ctx.arc(-2, -2, radius, 0, 2*Math.PI);
+    ctx.fillStyle = 'transparent';
     ctx.fill();
+    ctx.strokeStyle = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05).addColorStop(0, 'black');
+    ctx.lineWidth = radius*0.4;
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(2, 2);
+    ctx.lineTo(8, 8);
+    ctx.lineWidth = radius * 0.4;
+    ctx.stroke();
 }
